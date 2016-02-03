ecd7e82e469a7ff865c22a6f382eb753f6bc02c3
isaacstechnology <paul@isaacstechnology.com>
2016-02-03 10:15:40 +0100

    Initial Commit

---
+++ BrainHarmonics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BrainHarmonics
+//
+//  Created by Paul Isaac's on 03/02/16.
+//  Copyright © 2016 Paul Isaac's. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
