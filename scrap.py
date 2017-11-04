d93b4e09b40b1ee62d20962abc3b40487969f5c5
DevinBayly <baylyd@email.arizona.edu>
2017-11-04 15:29:21 +0100

    need to check opengl version, tesselation problems

--- scrap.py
+++ scrap.py
@@ -1,6 +1,19 @@
 import os,sys
 
-edited_files =[ele for ele in os.listdir(".") if not os.path.isdir(ele)]
+
+ignore = ".git build".split()
+
+def check_path(given):
+    for bad in ignore: 
+        print(bad,given)
+        print(type(bad),type(given))
+        if bad in given:
+            print(" caught ")
+            return False
+        print(" passed ")
+    return True
+
+edited_files = [os.path.join(place[0],file) for place in os.walk(".") for file in place[2] if check_path(place[0]) ]
 
 message = "need to check opengl version, tesselation problems"
 
