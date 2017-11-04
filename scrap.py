f25a1c954878ffdf8f1ac8c5f6c69b8da1cbb280
DevinBayly <baylyd@email.arizona.edu>
2017-11-04 12:32:30 +0100

    moving locations

--- scrap.py
+++ scrap.py
@@ -2,7 +2,7 @@ import os,sys
 
 edited_files =[ele for ele in os.listdir(".") if not os.path.isdir(ele)]
 
-message = "using offset for color, position, and orientation changes"
+message = "moving locations"
 
 command = "git add {}; git commit -m \"{}\"".format(" ".join(edited_files),message)
 
