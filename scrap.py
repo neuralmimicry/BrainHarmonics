459afb1de7c2fd4210525cecb1fb68791e46e265
DevinBayly <baylyd@email.arizona.edu>
2017-11-04 13:19:02 +0100

    made changes to shader.cpp to load tesselation things also

--- scrap.py
+++ scrap.py
@@ -1,8 +1,8 @@
-"import os,sys"
+import os,sys
 
 edited_files =[ele for ele in os.listdir(".") if not os.path.isdir(ele)]
 
-message = "moving locations"
+message = "made changes to shader.cpp to load tesselation things also"
 
 command = "git add {}; git commit -m \"{}\"".format(" ".join(edited_files),message)
 
